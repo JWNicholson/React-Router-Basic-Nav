407a9a85fa30f20651137bbb2ee10e3d8428c3f3
JW Nicholson <john.nicholson812@gmail.com>
2020-02-24 15:35:59 -0500

    added routes in App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import { Home, About, Contact, Navigation } from './components';
 import { Route } from "react-router-dom";
 
 const App = () => (
+
+
   <div>
     <Navigation />
+
+      <Route exact path="/" component={Home}/>
+
+      <Route path="/about" component={About}/>
+
+      <Route path="/contact" component={Contact} />
+
   </div>
 );
 
